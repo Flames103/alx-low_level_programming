b3b674d0b8e968e9fe707ca0edcb40933a08249a
flames103 <beeflamez1@gmail.com>
2022-12-08 16:43:09 -0800

    love you

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,41 +1,21 @@
-
-bomanaps
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x17-doubly_linked_lists/0-print_dlistint.c
-@bomanaps
-bomanaps task commit
- 1 contributor
-24 lines (20 sloc)  382 Bytes
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_dlistint - print elements of doubly linked list
- * @h: linked list
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
- * Return: number of nodes
+ * Return: The number of nodes in the list.
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nbr_nodes = 0;
-	const dlistint_t *current = h;
+	size_t nodes = 0;
 
-	while (current != NULL)
+	while (h)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
-		nbr_nodes++;
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
-	return (nbr_nodes);
+	return (nodes);
 }
